d9cb2548e4a1b33ffb4a6d0130d138e999a2b1db
mhmrez <mohammadahmadicoder@gmail.com>
2023-06-30 13:47:33 +0330

    Fix errors and bugs and update the test unit

--- contracts/Charity.sol
+++ contracts/Charity.sol
@@ -1,13 +1,12 @@
 //SPDX-License-Identifier:MIT
+    //forcibility sending ether
     pragma solidity ^0.8.0;
-    import "hardhat/console.sol";
 
     contract Charity{
-
         address public owner;
         uint48 public deadline;
         uint32 public counterNeedy;
-        uint32 public counterFunders = 0;
+        uint32 public counterFunders;
         uint48 public minCharityPrice = 0.0001 ether;
         uint32 public CounterTotal = 0;
 
@@ -17,7 +16,6 @@
         struct helper{
             uint32 Id;
             address payable Person;
-            bool voteStatus;
             uint256 value;
         }
         struct poor{
@@ -30,6 +28,7 @@
         }
         mapping(uint32 => helper) public helpers;
         mapping(uint32 => poor)public needy;
+        mapping(address => bool)public voteStatus;
 
         modifier OnlyOwner(){
             require(msg.sender == owner , "caller is not Owner!");
@@ -61,17 +60,17 @@
             require(needy[counterNeedy].condition == false , "This address has been entered");
             needy[counterNeedy] = poor(counterNeedy, payable(_addr) , 0 , true ,0 , 0);
             counterNeedy++;
-            console.log("debugging on sol" , _addr);
         }
             function cahangeLevelToPending()public OnlyOwner levelStart{
                 level = status.Pending;
         }
         function assist(uint24 _id)public payable levelPending{
             require(msg.value >= minCharityPrice , "There is little money coming in");
-            require(helpers[counterFunders].voteStatus == false , "This address has been voted");
+            require(voteStatus[msg.sender] == false , "This address has been voted");
             require(needy[_id].condition == true , "this address not true!");
             payable(address(this)).transfer(msg.value);
-            helpers[counterFunders] = helper(counterFunders , payable(msg.sender) , true , msg.value);
+            voteStatus[msg.sender] = true;
+            helpers[counterFunders] = helper(counterFunders , payable(msg.sender) , msg.value);
             CounterTotal++;
              needy[_id].vote++;
             counterFunders++;
@@ -85,11 +84,12 @@
             require(block.timestamp >= deadline , "The charity is active!");
             level = status.End; 
         }
-        function payOff(uint32 _id)public payable levelEnd{
+        function payEnd(uint32 _id)public payable levelEnd{
             //Possibility of attack (denial of service) 
             // for(uint24 x ; x <= counterNeedy;x++){
             // address payable person = needy[x].person;
             // person.transfer(needy[x].liquidyShare);
+            // }
             require(needy[_id].person == msg.sender , "caller not needy");
             if(needy[_id].vote == 0){
                 revert();
